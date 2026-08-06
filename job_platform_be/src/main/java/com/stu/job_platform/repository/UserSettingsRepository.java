e61b6bd97e781a899ea25254e1cba4f361708614
Learncode2004 <lukyhoc789@gmail.com>
2026-08-06 10:39:25 +0700

    Đã xóa phần cài đặt tài khoản

--- job_platform_be/src/main/java/com/stu/job_platform/repository/UserSettingsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stu.job_platform.repository;
-
-import com.stu.job_platform.entity.UserSettings;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserSettingsRepository extends JpaRepository<UserSettings, Integer> {
-}
